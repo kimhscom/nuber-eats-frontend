4d25b7dc1464a378c63bce8690e629092b03bafb
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-01-27 01:44:04 +0900

    14.3 Apollo Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="bg-black">
-      <h1 className="text-xl text-white">Hello Tailwind</h1>
-    </div>
-  );
+  return <div></div>
 }
 
 export default App;
