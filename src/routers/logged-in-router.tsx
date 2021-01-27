e16a354b29434538b820a2555bfac1f1bca1a378
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-01-27 16:13:23 +0900

    15.1 React Hook Form

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { isLoggedInVar } from "../apollo";
 
 export const LoggedInRouter = () => {
-    const onClick = () => {
-        isLoggedInVar(false);
-    };
+  const onClick = () => {
+    isLoggedInVar(false);
+  };
 
-    return (
-        <div>
-            <h1>Logged In</h1>
-            <button onClick={onClick}>Log Out</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Logged In</h1>
+      <button onClick={onClick}>Log Out</button>
+    </div>
+  );
 };
