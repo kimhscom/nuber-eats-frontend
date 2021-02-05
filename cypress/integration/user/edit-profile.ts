55e25e4012ef7e39e4f31d83ea8f862699e92dc2
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-02-05 22:02:26 +0900

    19.7 EditProfile E2E part Two

--- cypress/integration/user/edit-profile.ts
+++ cypress/integration/user/edit-profile.ts
@@ -12,6 +12,12 @@ describe("Edit Profile", () => {
   });
 
   it("can change email", () => {
+    user.intercept("POST", "http://localhost:4000/graphql", (req) => {
+      if (req.body?.operationName === "editProfile") {
+        // @ts-ignore
+        req.body?.variables?.input?.email = "kimhscom@kakao.com";
+      }
+    });
     user.visit("/edit-profile");
     user.findByPlaceholderText(/email/i).clear().type("new@email.com");
     user.findByRole("button").click();
