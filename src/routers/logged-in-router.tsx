e3a28ddba8602c6005fc7efd0631ce3812969c17
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-02-06 23:16:57 +0900

    20.7 Restaurant Dashboard

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -8,6 +8,7 @@ import { Restaurant } from "../pages/client/restaurant";
 import { Restaurants } from "../pages/client/restaurants";
 import { Search } from "../pages/client/search";
 import { AddRestaurant } from "../pages/owner/add-restaurants";
+import { MyRestaurant } from "../pages/owner/my-restaurant";
 import { MyRestaurants } from "../pages/owner/my-restaurants";
 import { ConfirmEmail } from "../pages/user/confirm-email";
 import { EditProfile } from "../pages/user/edit-profile";
@@ -39,6 +40,7 @@ const ClientRoutes = [
 const restaurantRoutes = [
   { path: "/", component: <MyRestaurants /> },
   { path: "/add-restaurant", component: <AddRestaurant /> },
+  { path: "/restaurants/:id", component: <MyRestaurant /> },
 ];
 
 export const LoggedInRouter = () => {
