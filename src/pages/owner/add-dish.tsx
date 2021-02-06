780c7f99f8ea6e3dad13d18ad10f56fc70de5e86
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-02-07 00:27:11 +0900

    20.9 Create Dish part Two

--- src/pages/owner/add-dish.tsx
+++ src/pages/owner/add-dish.tsx
@@ -1,6 +1,14 @@
 import { gql, useMutation } from "@apollo/client";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { useHistory, useParams } from "react-router-dom";
+import { Button } from "../../components/button";
+import {
+  createDish,
+  createDishVariables,
+} from "../../__generated__/createDish";
+import { MY_RESTAURANT_QUERY } from "./my-restaurant";
 
 const CREATE_DISH_MUTATION = gql`
   mutation createDish($input: CreateDishInput!) {
@@ -15,9 +23,89 @@ interface IParams {
   restaurantId: string;
 }
 
+interface IForm {
+  name: string;
+  price: string;
+  description: string;
+}
+
 export const AddDish = () => {
   const { restaurantId } = useParams<IParams>();
-  const [createDishMutation, { loading }] = useMutation(CREATE_DISH_MUTATION);
+  const history = useHistory();
+  const [createDishMutation, { loading }] = useMutation<
+    createDish,
+    createDishVariables
+  >(CREATE_DISH_MUTATION, {
+    refetchQueries: [
+      {
+        query: MY_RESTAURANT_QUERY,
+        variables: {
+          input: {
+            id: +restaurantId,
+          },
+        },
+      },
+    ],
+  });
+  const { register, handleSubmit, formState, getValues } = useForm<IForm>({
+    mode: "onChange",
+  });
 
-  return <h1>Add Dish</h1>;
+  const onSubmit = () => {
+    const { name, price, description } = getValues();
+
+    createDishMutation({
+      variables: {
+        input: {
+          name,
+          price: +price,
+          description,
+          restaurantId: +restaurantId,
+        },
+      },
+    });
+
+    history.goBack();
+  };
+
+  return (
+    <div className="container flex flex-col items-center mt-52">
+      <Helmet>
+        <title>Add Dish | Nuber Eats</title>
+      </Helmet>
+      <h4 className="font-semibold text-2xl mb-3">Add Dish</h4>
+      <form
+        className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          ref={register({ required: "Name is required." })}
+          className="input"
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
+        <input
+          ref={register({ required: "Price is required." })}
+          className="input"
+          type="number"
+          name="price"
+          min={0}
+          placeholder="Price"
+        />
+        <input
+          ref={register({ required: "Description is required." })}
+          className="input"
+          type="text"
+          name="description"
+          placeholder="Description"
+        />
+        <Button
+          loading={loading}
+          canClick={formState.isValid}
+          actionText="Create Dish"
+        />
+      </form>
+    </div>
+  );
 };
