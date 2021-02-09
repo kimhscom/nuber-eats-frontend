197531e3180bb5e659532941cd4f55e046df0ea8
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-02-10 01:50:26 +0900

    23.7 Driver Dashboard part Three

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -8,6 +8,8 @@ interface ICoords {
 
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
+  const [map, setMap] = useState<any>();
+  const [maps, setMaps] = useState<any>();
 
   const onSucces = ({
     coords: { latitude, longitude },
@@ -25,15 +27,24 @@ export const Dashboard = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [driverCoords.lat, driverCoords.lng]);
+
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
     map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
   };
 
   return (
     <div>
       <div
         className="overflow-hidden"
-        style={{ width: window.innerWidth, height: "95vh" }}
+        style={{ width: window.innerWidth, height: "50vh" }}
       >
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyCjWLlQWPNmpB3Wejb25D9-EqKu0BEkj4w" }}
@@ -41,10 +52,19 @@ export const Dashboard = () => {
             lat: 37.55,
             lng: 126.97,
           }}
-          defaultZoom={15}
+          defaultZoom={16}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={onApiLoaded}
-        ></GoogleMapReact>
+        >
+          <div
+            // @ts-ignore
+            lat={driverCoords.lat}
+            lng={driverCoords.lng}
+            className="text-lg"
+          >
+            🚖
+          </div>
+        </GoogleMapReact>
       </div>
     </div>
   );
