1e0b684755264ea84dc5dc35950f30cb0893ac13
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-02-11 01:35:23 +0900

    Update fetch uri

--- src/pages/owner/add-restaurants.tsx
+++ src/pages/owner/add-restaurants.tsx
@@ -92,7 +92,7 @@ export const AddRestaurant = () => {
       formBody.append("file", actualFile);
 
       const { url: coverImg } = await (
-        await fetch("http://localhost:4000/uploads/", {
+        await fetch("https://nuber-eats-clone-backend.herokuapp.com/uploads/", {
           method: "POST",
           body: formBody,
         })
