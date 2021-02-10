959609a8d7ce90bf71ab010023ff73475ef0c312
kimhscom <47877911+kimhscom@users.noreply.github.com>
2021-02-11 00:17:34 +0900

    Update URI path

--- src/apollo.ts
+++ src/apollo.ts
@@ -17,7 +17,7 @@ export const authTokenVar = makeVar(token);
 const wsLink = new WebSocketLink({
   uri:
     process.env.NODE_ENV === "production"
-      ? "wss://nuber-eats-backend.herokuapp.com/graphql"
+      ? "wss://nuber-eats-clone-backend.herokuapp.com/graphql"
       : `ws://localhost:4000/graphql`,
   options: {
     reconnect: true,
@@ -30,7 +30,7 @@ const wsLink = new WebSocketLink({
 const httpLink = createHttpLink({
   uri:
     process.env.NODE_ENV === "production"
-      ? "https://nuber-eats-backend.herokuapp.com/graphql"
+      ? "https://nuber-eats-clone-backend.herokuapp.com/graphql"
       : "http://localhost:4000/graphql",
 });
 
